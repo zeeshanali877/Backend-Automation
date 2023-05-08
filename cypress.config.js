4e492d0bdb3afcd64283d4bb11b4cd5a4501ddc2
Syed Zeeshan Ali <syedzeeshanali@MP18-SZeeshanA-SQA-521.local>
2023-05-08 16:06:07 +0500

    reverting changes

--- cypress.config.js
+++ cypress.config.js
@@ -4,21 +4,21 @@ const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      addMatchImageSnapshotPlugin();
-      return config;
+      // implement node event listeners here
+      
     },
     specPattern: 'cypress/integration/**/*.js',
     
-      // "reporter": "mochawesome",
-      // "reporterOptions": {
-      //   "reportDir": "cypress/reports/mochawesome-report",
-      //   "overwrite": false,
-      //   "html": false,
-      //   "json": true
-      // },
-      // "video": false,
-      // "plugins": {
-      //   "mochawesome-merge": {}
-      // }
+      "reporter": "mochawesome",
+      "reporterOptions": {
+        "reportDir": "cypress/reports/mochawesome-report",
+        "overwrite": false,
+        "html": false,
+        "json": true
+      },
+      "video": false,
+      "plugins": {
+        "mochawesome-merge": {}
+      }
     }
   });
